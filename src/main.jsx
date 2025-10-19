6a43dd13320f674a2ed2bb33682d814257bc0ffe
Asante Derrick <derrasan01@gmail.com>
2025-10-19 03:27:59 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import './index.css'
 
 // This line is crucial for the map tiles to appear correctly.
 import 'leaflet/dist/leaflet.css';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
